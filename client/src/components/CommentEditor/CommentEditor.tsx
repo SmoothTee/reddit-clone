846f65208df588d974f0d257aa440258b5f17601
Joey Liew <joeyl@tutanota.com>
2020-09-17 22:07:55 +0200

    Add auth request to CommentEditor

--- client/src/components/CommentEditor/CommentEditor.tsx
+++ client/src/components/CommentEditor/CommentEditor.tsx
@@ -2,6 +2,8 @@ import React, { FormEvent, useState } from "react";
 import ReactQuill from "react-quill";
 import { useDispatch } from "react-redux";
 import { createCommentAction } from "../../redux/comment/actions";
+import { useTypedSelector } from "../../redux/hooks";
+import { showModal } from "../../redux/modal/actions";
 import { Button } from "../Button";
 
 import styles from "./CommentEditor.module.css";
@@ -15,6 +17,10 @@ interface CommentEditorProps {
 export const CommentEditor = ({ postId, parentId, cb }: CommentEditorProps) => {
   const dispatch = useDispatch();
   const [body, setBody] = useState("");
+
+  const isAuthenticated = useTypedSelector(
+    (state) => state.auth.isAuthenticated
+  );
 
   const callback = () => {
     cb();
@@ -32,10 +38,28 @@ export const CommentEditor = ({ postId, parentId, cb }: CommentEditorProps) => {
     );
   };
 
-  return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <ReactQuill theme="snow" value={body} onChange={setBody} />
-      <Button disabled={!body.length}>Create Comment</Button>
-    </form>
-  );
+  if (isAuthenticated) {
+    return (
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <ReactQuill theme="snow" value={body} onChange={setBody} />
+        <Button disabled={!body.length}>Create Comment</Button>
+      </form>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <span className={styles.text}>
+          Log in or Register to leave a comment.
+        </span>
+        <div className={styles.buttons}>
+          <Button onClick={() => dispatch(showModal("LoginModal"))}>
+            Log In
+          </Button>
+          <Button onClick={() => dispatch(showModal("RegisterModal"))}>
+            Sign Up
+          </Button>
+        </div>
+      </div>
+    );
+  }
 };
